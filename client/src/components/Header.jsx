e10da13ed7ddbc087d3c742713e1f5d24027ec8d
Xica Felix <xicasfelix@gmail.com>
2023-03-11 16:27:22 -0500

    creating landing page

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { Header } from "antd/es/layout/layout";
+import { Image } from "antd";
+
+import logo from '/Users/Xicafelix/Development/code/Phase-4/helping-hands/client/src/Assets/Helping.png';
+
+function AppHeader(){
+
+    return(
+        <Header style={{height:150}}>
+            <div className="logo">
+                <Image width={150} src={logo}/>
+            </div>
+        </Header>
+    )
+}
+
+export default AppHeader;
---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,20 @@
+import {Layout} from 'antd'
+import AppHeader from './Header';
+
+
+
+
+function LandingPage(){
+
+
+
+    return(
+        <Layout className='layout'>
+          <AppHeader/> 
+
+        </Layout>
+    )
+
+}
+
+export default LandingPage;
