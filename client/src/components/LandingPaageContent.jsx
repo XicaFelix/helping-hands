6a373692cf6ee3b285cc21716d4248f43d7437e9
Xica Felix <xicasfelix@gmail.com>
2023-03-11 17:18:15 -0500

    adding landing page image, logo, developing landing page

--- client/src/components/LandingPaageContent.jsx
+++ client/src/components/LandingPaageContent.jsx
@@ -1,1 +1,15 @@
 import { Content } from 'antd';
+import backgroundImage from '/Users/Xicafelix/Development/code/Phase-4/helping-hands/client/src/Assets/pexels-kampus-production-7551671.jpg'
+
+function LandingContent(){
+
+    return(
+        <Content>
+            <div className='background-image' style={{backgroundImage:`url(${backgroundImage})`}}>
+
+            </div>
+        </Content>
+    );
+}
+
+export default LandingContent;
