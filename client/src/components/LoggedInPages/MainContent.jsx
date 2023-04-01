88a39ceec9906f1b8d65be7a610fc9793ccce4ab
Xica Felix <xicasfelix@gmail.com>
2023-04-01 15:42:32 -0400

    wiring appointment tab to render appointments

--- client/src/components/LoggedInPages/MainContent.jsx
+++ client/src/components/LoggedInPages/MainContent.jsx
@@ -6,19 +6,21 @@ import Medications from './Medications';
 
 function MainContent({user, setUser}){
     let medsList;
+    let apptList;
     console.log(user)
 
     if(user !== null){
         medsList = user.medications.map((medication)=> <Medications key={medication.id} medication={medication}/>)
+        apptList = user.appointments.map((appointment)=> <Appointment key={appointment.id} appointment={appointment}/>)
     }
 
     return(
         <Tabs type='card'>
             <TabPane tab='Medications' key={1}>
-               {user !==null ? medsList : <h3> bloop </h3>}
+               {user !==null ? medsList : <h3> System error</h3>}
             </TabPane>
             <TabPane tab='Appointments' key={2}>
-                <Appointment/>
+                {apptList}
             </TabPane>
         </Tabs>
     );
